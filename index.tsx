db19be8ca01190f04054a05380c043bd2242e087
Freddy Bremseth <freddy.bremseth@gmail.com>
2026-05-11 09:09:39 +0200

    Load professional navigation links on public site

--- index.tsx
+++ index.tsx
@@ -4,6 +4,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './tailwind.css';
 
+const loadProfessionalLinks = () => {
+  if (typeof document === 'undefined') return;
+  if (document.querySelector('script[data-donaanna-links="true"]')) return;
+
+  const script = document.createElement('script');
+  script.src = '/donaanna-site-links.js';
+  script.defer = true;
+  script.dataset.donaannaLinks = 'true';
+  document.body.appendChild(script);
+};
+
+loadProfessionalLinks();
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
