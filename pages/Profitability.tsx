b09cce5e989d1d5b0c79be53497f8fe491213a18
Freddy Bremseth <freddy.bremseth@gmail.com>
2026-06-02 22:12:02 +0200

    Use Olivia schema backed profitability dashboard

--- pages/Profitability.tsx
+++ pages/Profitability.tsx
@@ -1,6 +1,5 @@
-
 import React from 'react';
-import ProfitabilityDashboard from '../components/ProfitabilityDashboard';
+import ProfitabilityOliviaDashboard from '../components/ProfitabilityOliviaDashboard';
 import { Language, Parcel } from '../types';
 
 interface ProfitabilityPageProps {
@@ -11,7 +10,7 @@ interface ProfitabilityPageProps {
 const ProfitabilityPage: React.FC<ProfitabilityPageProps> = ({ language, parcels }) => {
   return (
     <div className="max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:px-8">
-      <ProfitabilityDashboard language={language} parcels={parcels} />
+      <ProfitabilityOliviaDashboard language={language} parcels={parcels} />
     </div>
   );
 };
